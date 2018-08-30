37b007f4b9233fe3c6f33dec2b21b6235fda71eb
CloudBarista <lordbaldwin@Apples-MacBook-Pro-2.local>
2018-08-30 23:59:16 +0530

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import 'flatpickr/dist/flatpickr.css';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarModule } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+
+
+import {MatButtonToggleModule,
+   MatButtonModule,MatDividerModule,
+    MatExpansionModule,MatTabsModule} from '@angular/material'
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +31,20 @@ import { CalendarComponent } from './calendar/calendar.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    NgbModalModule.forRoot(),
+FlatpickrModule.forRoot(),
+CalendarModule.forRoot(),
+
+    //AngularMaterials
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatTabsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
